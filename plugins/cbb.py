ace032d78eb9e7d3708f42fb7fbb4f7bb855cf15
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2024-01-17 22:06:02 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Channel : @movies_x_store\n○ Channel List : @sid_op</b>",
+            text = f"<b>○Bot Owner : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Channel : @new_movie_i24\n○ Bot Maker : @MriderDm</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
